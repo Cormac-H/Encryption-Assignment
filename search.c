3238285521fdbd73f0d5f536233e423aa845b0a0
PRAKET-KUMAR <praketkumar@gmail.com>
2023-05-09 17:18:33 +1000

    Updated search algorithms

--- search.c
+++ search.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILE_LENGTH 200
+
 /*******************************************************************************
  * This function performs a linear search for content found in a file
  *
@@ -7,9 +13,24 @@
  * - none
  * author: 
 *******************************************************************************/
-void binarySearch()
+void linearSearch(char* fileName, char* content)
 {
+    FILE* file = fopen(fileName, "r");
+    char buffer[MAX_FILE_LENGTH];
+    int lineNumber = 0;
+    int found = 0;
 
+    while(fgets (buffer, MAX_FILE_LENGTH)){
+        lineNumber++;
+        if(strstr(buffer, content)){
+            printf("'%s' found on line %d using linear search\n", content, lineNumber);
+            found = 1;
+        }
+    }
+    if(!found){
+        printf("'%s' not found in the file using linear search\n", content);
+    }
+    fclose(file);
 }
 
 /*******************************************************************************
@@ -21,7 +42,7 @@ void binarySearch()
  * - none
  * author: 
 *******************************************************************************/
-void linearSearch()
+void binarySearch()
 {
 
 }
